87fd40ac6a49c68679afa51febaeb8e6771331a7
zer0pwn <zer0pwn@riseup.net>
2019-03-01 14:02:28 -0600

    Update spysecli.py

--- tests/spysecli.py
+++ tests/spysecli.py
@@ -1,14 +1,5 @@
 #!/usr/bin/python3
 
-# Usage examples
-#
-# ./spysecli.py -target hackerone.com -param domain --subdomains-aggregate
-# ./spysecli.py -target "52.14.144.171" -param ip --domains-on-ip
-# ./spysecli.py -target "52.14.144.0/24" -param cidr --dns-a
-#
-# you can easily pipe the subdomains-aggregate results to aquatone 
-# try it out :ppp
-
 from pprint import pprint
 from spyse import spyse
 import argparse
@@ -26,11 +17,183 @@ parser.add_argument('--dns-aaaa', help="show dns aaaa records", action="store_tr
 parser.add_argument('--dns-ns', help="show dns ns records", action="store_true")
 parser.add_argument('--dns-a', help="show dns a records", action="store_true")
 parser.add_argument('--dns-txt', help="show dns txt records", action="store_true")
+parser.add_argument('--dns-all', help="show all dns records", action="store_true")
 parser.add_argument('--domains-with-same-ns', help="show domains with same ns", action="store_true")
 parser.add_argument('--domains-using-as-mx', help="show domains using as mx", action="store_true")
 parser.add_argument('--domains-on-ip', help="show domains on ip", action="store_true")
 parser.add_argument('--subdomains-aggregate', help="show subdomains aggregate", action="store_true")
 args = parser.parse_args()
+
+s = spyse()
+
+def get_dns_ptr(target, param, page, raw=False):
+	if(raw == True):
+		return(s.dns_ptr(target, param=param, page=page))
+	else:
+		data = s.dns_ptr(target, param=param, page=page)
+		retval = ""
+
+		for record in data['records']:
+			retval += "PTR RECORD @ {} FROM HOSTNAME {}\n".format(
+				record['ip']['ip'],
+				record['hostname']
+			)
+		return retval
+	return False
+
+def get_dns_soa(target, param, page, raw=False):
+	if args.raw:
+		return print(s.dns_soa(target, param=param, page=page))
+	else:
+		data = s.dns_soa(target, param=param, page=page)
+		retval = ""
+		for record in data['records']:
+			retval += "SOA RECORD @ {} FROM {} WITH SERIAL {}\n".format(
+				record['domain']['domain'],
+				record['domain']['ip']['ip'],
+				record['serial']
+			)
+		return retval
+	return False
+
+def get_dns_mx(target, param, page, raw=False):
+	if args.raw:
+		return(s.dns_mx(target, param=param, page=page))
+	else:
+		data = s.dns_mx(target, param=param, page=page)
+		retval = ""
+		for record in data['records']:
+			retval += "MX RECORD @ {} FROM IP {}\n".format(
+				record['mx_domain']['domain'],
+				record['mx_domain']['ip']['ip']
+			)
+		return retval
+	return False
+
+def get_dns_aaaa(target, param, page, raw=False):
+	if args.raw:
+		return s.dns_aaaa(target, param=param, page=page)
+	else:
+		data = s.dns_aaaa(target, param=param, page=page)
+		retval = ""
+		for record in data['records']:
+			retval += "AAAA RECORD @ {} FROM IP {}\n".format(
+				record['domain']['domain'],
+				record['ipv6']
+			)
+		return retval
+	return False
+
+def get_dns_ns(target, param, page, raw=False):
+	if args.raw:
+		return s.dns_ns(target, param=param, page=page)
+	else:
+		data = s.dns_ns(target, param=param, page=page)
+		retval = ""
+		for record in data['records']:
+			retval += "NS RECORD @ {} FROM {}\n".format(
+				record['ns_domain']['domain'],
+				record['ns_domain']['ip']['ip']
+			)
+		return retval
+	return False
+
+def get_dns_a(target, param, page, raw=False):
+	if args.raw:
+		print(s.dns_a(target, param=param, page=page))
+		sys.exit()
+	else:
+		data = s.dns_a(target, param=param, page=page)
+		retval = ""
+		for record in data['records']:
+			retval += "A RECORD @ {} FROM {}\n".format(
+				record['domain']['domain'],
+				record['ip']['ip']
+			)
+		return retval
+	return False
+
+def get_dns_txt(target, param, page, raw=False):
+	if args.raw:
+		return s.dns_txt(target, param=param, page=page)
+	else:
+		data = s.dns_txt(target, param=param, page=page)
+		retval = "TXT RECORDS FROM {}\n".format(target)
+		for record in data['records']:
+			retval += '> {}\n'.format(record['data'])
+		return retval
+	return False
+
+
+def get_domains_with_same_ns(target, param, page, raw=False):
+	return s.domains_with_same_ns(target, param, page)
+
+
+def get_domains_using_as_mx(target, param, page, raw=False):
+	if args.raw:
+		return s.domains_using_as_mx(target, param=param, page=page)
+	else:
+		data = s.domains_using_as_mx(target, param=param, page=page)
+		retval = ""
+		for record in data['records']:
+			retval += "{} USING SAME MX AS {} ON IP {}\n".format(
+				record['domain'],
+				args.target,
+				record['ip']['ip']
+			)
+		return retval
+	return False
+
+def get_domains_on_ip(target, param, page, raw=False):
+	if args.raw:
+		return s.domains_on_ip(target, param=param, page=page)
+	else:
+		retval = ""
+		data = s.domains_on_ip(target, param=param, page=page)
+		for record in data['records']:
+			retval += "{}\n".format(record['domain'])
+		return retval
+	return False
+
+def get_subdomains_aggregate(target, param, page, raw=False):
+	if args.raw:
+		return s.subdomains_aggregate(target, param=param, page=page)
+	else:
+		retval = ""
+		data = s.subdomains_aggregate(target, param=param, page=page)['cidr']
+		keys = data.keys()
+
+		for key in keys:
+			domains = data[key]['results']
+			for d in domains:
+				domain = d['data']['domains']
+				if len(domain) > 1:
+					for i in domain:
+						retval += "{}\n".format(i)
+				else:
+					retval += "{}\n".format(domain[0])
+		return retval
+	return False
+
+def get_dns_all(target, param, page, raw=False):
+	data = ""
+	if args.raw:
+		data += get_dns_ptr(target, param=param, page=page, raw=True)
+		data += get_dns_soa(target, param=param, page=page, raw=True)
+		data += get_dns_mx(target, param=param, page=page, raw=True)
+		data += get_dns_aaaa(target, param=param, page=page, raw=True)
+		data += get_dns_a(target, param=param, page=page, raw=True)
+		data += get_dns_ns(target, param=param, page=page, raw=True)
+		data += get_dns_txt(target, param=param, page=page, raw=True)
+	else:
+		data += get_dns_ptr(target, param=param, page=page)
+		data += get_dns_soa(target, param=param, page=page)
+		data += get_dns_mx(target, param=param, page=page)
+		data += get_dns_aaaa(target, param=param, page=page)
+		data += get_dns_a(target, param=param, page=page)
+		data += get_dns_ns(target, param=param, page=page)
+		data += get_dns_txt(target, param=param, page=page)
+	return data
 
 if args.target and args.param:
 	if args.page:
@@ -41,152 +204,38 @@ if args.target and args.param:
 	target = args.target
 	param = args.param
 
-	s = spyse()
-
-	if args.subdomains_aggregate:
-
-		if args.raw:
-			print(s.subdomains_aggregate(args.target, param=args.param, page=page))
-			sys.exit()
-
-		data = s.subdomains_aggregate(args.target, param=args.param, page=page)['cidr']
-		keys = data.keys()
-
-		for key in keys:
-			domains = data[key]['results']
-			for d in domains:
-				domain = d['data']['domains']
-				if len(domain) > 1:
-					for i in domain:
-						print(i)
-				else:
-					print(domain[0])
-
 	if args.dns_ptr:
-
-		if args.raw:
-			print(s.dns_ptr(args.target, param=args.param, page=page))
-			sys.exit()
-
-		data = s.dns_ptr(args.target, param=args.param, page=page)
-
-		for record in data['records']:
-			print("PTR RECORD @ {} FROM HOSTNAME {}".format(
-				record['ip']['ip'],
-				record['hostname']
-			))
+		print(get_dns_ptr(args.target, args.param, page, raw=args.raw))
 
 	if args.dns_soa:
-		if args.raw:
-			print(s.dns_soa(args.target, param=args.param, page=page))
-			sys.exit()
-
-		data = s.dns_soa(args.target, param=args.param, page=page)
-
-		for record in data['records']:
-			print("SOA RECORD @ {} FROM {} WITH SERIAL {}".format(
-				record['domain']['domain'],
-				record['domain']['ip']['ip'],
-				record['serial']
-				))
+		print(get_dns_soa(args.target, args.param, page, raw=args.raw))
 
 	if args.dns_mx:
-
-		if args.raw:
-			print(s.dns_mx(args.target, param=args.param, page=page))
-			sys.exit()
-
-		data = s.dns_mx(args.target, param=args.param, page=page)
-		for record in data['records']:
-			print("MX RECORD @ {} FROM IP {}".format(
-				record['mx_domain']['domain'],
-				record['mx_domain']['ip']['ip']
-			))
+		print(get_dns_mx(args.target, args.param, page, raw=args.raw))
 
 	if args.dns_aaaa:
-
-		if args.raw:
-			print(s.dns_aaaa(args.target, param=args.param, page=page))
-			sys.exit()
-
-		data = s.dns_aaaa(args.target, param=args.param, page=page)
-
-		for record in data['records']:
-			print("AAAA RECORD @ {} FROM IP {}".format(
-				record['domain']['domain'],
-				record['ipv6']
-			))
+		print(get_dns_aaaa(args.target, args.param, page, raw=args.raw))
 
 	if args.dns_ns:
-
-		if args.raw:
-			print(s.dns_ns(args.target, param=args.param, page=page))
-			sys.exit()
-
-		data = s.dns_ns(args.target, param=args.param, page=page)
-
-		for record in data['records']:
-			print("NS RECORD @ {} FROM {}".format(
-				record['ns_domain']['domain'],
-				record['ns_domain']['ip']['ip']
-			))
+		print(get_dns_ns(args.target, args.param, page, raw=args.raw))
 
 	if args.dns_a:
-
-		if args.raw:
-			print(s.dns_a(args.target, param=args.param, page=page))
-			sys.exit()
-
-		data = s.dns_a(args.target, param=args.param, page=page)
-
-		for record in data['records']:
-			print("A RECORD @ {} FROM {}".format(
-				record['domain']['domain'],
-				record['ip']['ip']
-			))
+		print(get_dns_a(args.target, args.param, page, raw=args.raw))
 
 	if args.dns_txt:
-
-		if args.raw:
-			print(s.dns_txt(args.target, param=args.param, page=page))
-			sys.exit()
-
-		data = s.dns_txt(args.target, param=args.param, page=page)
-		print("TXT RECORDS FROM {}".format(args.target))
-		for record in data['records']:
-			print('> ', record['data'])
+		print(get_dns_txt(args.target, args.param, page, raw=args.raw))
 
 	if args.domains_with_same_ns:
-
-		if args.raw:
-			print(s.domains_with_same_ns(args.target, param=args.param, page=page))
-			sys.exit()
-
-		data = s.domains_with_same_ns(args.target, param=args.param, page=page)
-		print(data)
+		print(get_domains_with_same_ns(args.target, args.param, page, raw=args.raw))
 
 	if args.domains_using_as_mx:
-
-		if args.raw:
-			print(s.domains_using_as_mx(args.target, param=args.param, page=page))
-			sys.exit()
-
-		data = s.domains_using_as_mx(args.target, param=args.param, page=page)
-
-		for record in data['records']:
-			print("{} USING SAME MX AS {} ON IP {}".format(
-				record['domain'],
-				args.target,
-				record['ip']['ip']
-			))
+		print(get_domains_using_as_mx(args.target, args.param, page, raw=args.raw))
 
 	if args.domains_on_ip:
+		print(get_domains_on_ip(args.target, args.param, page, raw=args.raw))
 
-		if args.raw:
-			print(s.domains_on_ip(args.target, param=args.param, page=page))
-			sys.exit()
+	if args.subdomains_aggregate:
+		print(get_subdomains_aggregate(args.target, args.param, page, raw=args.raw))
 
-		data = s.domains_on_ip(args.target, param=args.param, page=page)
-
-		for record in data['records']:
-			print(record['domain'])
+	if args.dns_all:
+		print(get_dns_all(args.target, args.param, page, raw=args.raw))
