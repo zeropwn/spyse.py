2f0157c542a04f7d0edc7263b957fd59e2f4bf44
zeropwn <zer0pwn@riseup.net>
2019-06-25 02:16:52 -0500

    sync with pip package

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
-
+import setuptools
 from distutils.core import setup
 
 setup(name='spyse.py',
-      version='1.0',
+      version='0.10',
       description='API wrapper & client for spyse.com',
       author='Dominik Penner',
       author_email='zer0pwn@riseup.net',
       url='https://www.twitter.com/zer0pwn',
       packages=['spyse'],
-      scripts=['bin/spyse']
+      scripts=['bin/spyse'],
+      install_requires=['requests']
      )
