875d874e20dfb3b53c084926573f2815a4334fb5
zeropwn <zer0pwn@riseup.net>
2019-06-26 13:53:25 -0500

    Modify setup & README & sync to pypi.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(name='spyse.py',
-      version='0.13.1',
+      version='0.13.2',
       description='API wrapper & client for spyse.com',
       long_description=long_description,
       long_description_content_type='text/markdown',
